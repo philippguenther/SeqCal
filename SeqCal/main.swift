057e548a74f0b5ac3b9fda717e49862b251f6097
Philipp Günther <p.guenther@campus.tu-berlin.de>
2017-02-08 20:03:32 +0100

    Initial Commit

---
+++ SeqCal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SeqCal
+//
+//  Created by Philipp Günther on 08.02.2017.
+//  Copyright © 2017 Philipp Günther. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
